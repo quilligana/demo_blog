e2a98057febe7be4575327c00622dfa41d12b0c1
Aidan Quilligan <a.quilligan@gmail.com>
2014-02-26 01:32:23 +0000

    Initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :author, presence: true, length: { maximum: 50 }
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoBlog::Application.config.session_store :cookie_store, key: '_demo_blog_session'
