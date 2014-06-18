aac71ffe2390a962670c1acd34ffd2ac47eeba25
Aidan Quilligan <a.quilligan@gmail.com>
2014-06-19 00:00:09 +0100

    Post specs and model updated

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
 
-	validates :title, presence: true, length: { minimum: 5 }
+	validates :title, presence: true, length: { minimum: 4 }
 	validates :author, presence: true, length: { maximum: 50 }
+  validates :text, presence: true, length: { minimum: 10 }
 
   belongs_to :user
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe Post do
   
-  it { should respond_to :title }
-  it { should respond_to :author }
   it { should validate_presence_of :title }
   it { should validate_presence_of :author }
+  it { should validate_presence_of :text }
+
+  it { should belong_to :user }
 
 end
