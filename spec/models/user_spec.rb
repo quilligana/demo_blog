c9fa791cafa806da8c54fdad74a9adb91b467b1b
Aidan Quilligan <a.quilligan@gmail.com>
2014-06-19 00:11:15 +0100

    Fixed failing user spec email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
 
   it { should validate_presence_of :firstname }
   it { should validate_presence_of :lastname }
-  it { should validate_uniqueness_of :email }
+  it { should validate_presence_of :email }
   it { should validate_presence_of :password}
 
   it { should have_many(:posts).dependent(:destroy) }
